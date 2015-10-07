40de473a19afbeb4bec462866b6cd0ab8b5e8ad5
matthacker <matthacker.alex@gmail.com>
2015-10-07 17:29:44 +0200

    Changed to agentspart 2 instead of one big file

---
+++ Delopgave1/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Delopgave7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        Clock clock = new Clock();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            spClock.DataContext = clock;
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            clock.Update();
+        }
+    }
+}
